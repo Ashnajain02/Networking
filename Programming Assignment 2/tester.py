289f47bb7d45aae45c283eb9f62b497bf91d7468
Ashna Jain <36568527+Ashnajain02@users.noreply.github.com>
2024-04-11 12:44:31 -0700

    Add files via upload

---
+++ Programming Assignment 2/tester.py
@@ -0,0 +1,103 @@
+# #message = ['0', '0', 'When', 'in', 'the', 'Course', 'o', '00972']
+# message = '0 0 When in the Course o 00972'
+# msg_SEQ = message[0] + "/"
+# msg_ACK = message[2] + "/"
+# msg_payload = message[:25] + "/"
+# msg_checksum = message[25:] + "/"
+
+# msg_SEQ = message[0]
+# msg_ACK = message[3]
+# msg_payload = message[2:len(message)-1]
+# msg_checksum = message[len(message)-1]
+
+# print(msg_SEQ, msg_ACK, msg_payload, msg_checksum)
+
+message = "HelloAshnaJainIsTheHalfBloodPrince"
+pointer = 0
+
+while pointer <= 8:
+    print("message: ", message[pointer: pointer + 2])
+    pointer += 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#https://github.com/hugoamaralsantana/RDT3.0-In-Python/blob/7d6f5e401f1bbf2c3d5b43494fad9db56d7df309/receiver.py
+
+
